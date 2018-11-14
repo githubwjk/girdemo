da0422a09dbb9b622ca6b1aa79276b9bfac0ff73
wjk <1812733875@qq.com>
2018-11-14 22:59:28 +0800

    myself2的第二次修改

--- src/main/java/text.java
+++ src/main/java/text.java
@@ -7,5 +7,6 @@ public class text {
 
         System.out.println("我就是你爸爸");
         System.out.println("第三个分支");
+        System.out.println("高雪艳  啊  高雪艳   你这是怎么了    对了 我好想不可以这样写  这样是有问题的  你说是不是 这样 说真的  我有点喜欢你  你就是不知道");
     }
 }
